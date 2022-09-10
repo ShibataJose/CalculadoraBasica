4948bddc24e6038dc299986defde4eda4a4e50f5
Shibata <jose_shibata@hotmail.com>
2022-09-10 13:36:38 -0300

    Verificação e Conversão de double para int, alterações no modo de receber dados do input e customização dos botões

--- app/src/main/java/com/example/calculadorabasica/MainActivity.java
+++ app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     EditText valor2;
     TextView campoResultado;
     boolean verificaDados;
+    double num1, num2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         valor1 = findViewById(R.id.valor1);
         valor2 = findViewById(R.id.valor2);
         campoResultado = findViewById(R.id.resultado);
-
     }
 
     Operacoes operacoes = new Operacoes();
@@ -31,48 +31,40 @@ public class MainActivity extends AppCompatActivity {
     public void botaoSoma(View v){
         verificaDados = dadosVerificados();
         if(verificaDados){
-            double num1 = Double.valueOf(valor1.getText().toString());
-            double num2 = Double.valueOf(valor2.getText().toString());
-            System.out.println(operacoes.soma(num1,num2));
-            campoResultado.setText(""+operacoes.soma(num1,num2));
+            recebeNumeros();
+            verificaInteiros(operacoes.soma(num1,num2), operacoes.soma(num1,num2));
         }
     }
 
     public void botaoSub(View v){
         verificaDados = dadosVerificados();
         if(verificaDados) {
-            double num1 = Double.valueOf(valor1.getText().toString());
-            double num2 = Double.valueOf(valor2.getText().toString());
-            campoResultado.setText("" + operacoes.subtracao(num1, num2));
+            recebeNumeros();
+            verificaInteiros(operacoes.subtracao(num1,num2), operacoes.subtracao(num1,num2));
         }
     }
 
     public void botaoMult(View v){
         verificaDados = dadosVerificados();
         if(verificaDados) {
-            double num1 = Double.valueOf(valor1.getText().toString());
-            double num2 = Double.valueOf(valor2.getText().toString());
-            campoResultado.setText("" + operacoes.multiplicacao(num1, num2));
+            recebeNumeros();
+            verificaInteiros(operacoes.multiplicacao(num1,num2), operacoes.multiplicacao(num1,num2));
         }
     }
 
     public void botaoDiv(View v){
         verificaDados = dadosVerificados();
         if(verificaDados) {
-            double num1 = Double.valueOf(valor1.getText().toString());
-            double num2 = Double.valueOf(valor2.getText().toString());
-            double resultadoDiv = operacoes.divisao(num1, num2);
-            campoResultado.setText(Double.toString(resultadoDiv));
+            recebeNumeros();
+            verificaInteiros(operacoes.divisao(num1,num2), operacoes.divisao(num1,num2));
         }
     }
 
     public void botaoExpo(View v) {
         verificaDados = dadosVerificados();
         if(verificaDados){
-            double num1 = Double.valueOf(valor1.getText().toString());
-            double num2 = Double.valueOf(valor2.getText().toString());
-            double resultadoExpo = operacoes.exponenciacao(num1, num2);
-            campoResultado.setText("" + resultadoExpo);
+            recebeNumeros();
+            verificaInteiros(operacoes.exponenciacao(num1,num2), operacoes.exponenciacao(num1,num2));
         }
     }
 
@@ -98,8 +90,26 @@ public class MainActivity extends AppCompatActivity {
             valor2.requestFocus();
             retorno = false;
         }
-
         return retorno;
     }
 
+    public boolean verificaInteiros(double resultado, double resultado2){
+
+        boolean retorno;
+
+        if(resultado%1==0){
+            int convNum = (int)resultado;
+            campoResultado.setText(""+convNum);
+            retorno = true;
+        }else {
+            campoResultado.setText("" + resultado2);
+            retorno = false;
+        }
+        return retorno;
+    }
+
+    public void recebeNumeros(){
+        num1 = Double.valueOf(valor1.getText().toString());
+        num2 = Double.valueOf(valor2.getText().toString());
+    }
 }
